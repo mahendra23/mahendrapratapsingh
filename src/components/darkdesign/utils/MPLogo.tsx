a7a8a8f11d6e24818867defc8ac6883f4e1ab59d
mahendra23 <mahendra63023@outlook.com>
2025-09-13 14:01:02 +1200

    auto hide logo

--- src/components/darkdesign/utils/MPLogo.tsx
+++ src/components/darkdesign/utils/MPLogo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MPLogo.scss";
 import { MY_LOGO } from "../../../common/constants";
 
@@ -7,10 +7,31 @@ interface MPLogoProps {
 }
 
 export const MPLogo: React.FC<MPLogoProps> = ({onClick}) => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        // scrolling down -> hide
+        setShow(false);
+      } else {
+        // scrolling up -> show
+        setShow(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
   return (
-    <button className="mp-initials-icon" onClick={onClick} title={"Icon"}>
-      <img src={MY_LOGO} alt={"MP-LOGO"} className="mp-initials-icon-img" />
+    <button className={`mp-initials-icon ${show ? "show" : "hide"}`} onClick={onClick}>
+      <img src={MY_LOGO} alt="MP Logo" className="mp-initials-icon-img" />
     </button>
   );
-};
+}

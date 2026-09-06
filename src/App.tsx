07cf87cb262fea3572e3b28b2f387de814d56059
mahendra23 <mahendra63023@outlook.com>
2026-09-06 22:34:23 +1200

    removed preloder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./common/App.scss";
-import "@coreui/coreui/dist/css/coreui.min.css";\
+import "@coreui/coreui/dist/css/coreui.min.css";
 import LightHomeBase from "./components/lightdesign/lighthomebase/LightHomeBase";
 import DarkHomeBase from "./components/darkdesign/darkhomebase/DarkHomeBase";
 import { Header } from "./components/header/Header";
